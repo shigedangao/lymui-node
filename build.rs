12706c7ad04c6372d6b0b0aed8ecca5c8ce4b1f8
shigedangao <marc.inthaamnouay@gmail.com>
2026-05-25 17:51:40 +0200

    add cbindgen.toml

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
 extern crate cbindgen;
-
+use cbindgen::{Builder, Config};
 use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let config = Config::from_root_or_default(&crate_dir);
 
-    cbindgen::Builder::new()
+    Builder::new()
         .with_crate(crate_dir)
+        .with_config(config)
         .with_language(cbindgen::Language::C)
         .generate()
         .expect("Unable to generate bindings")
